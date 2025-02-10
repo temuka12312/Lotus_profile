3030847ba780f554ed37e68ef3feca162f8550e9
Your Name <you@example.com>
2025-02-10 18:10:35 +0800

    database holboson

--- midsite/settings.py
+++ midsite/settings.py
@@ -86,8 +86,12 @@ WSGI_APPLICATION = 'midsite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',  # Replace with your database name
+        'USER': 'postgres',  # Replace with your database user
+        'PASSWORD': 'Temka0506!',  # Replace with your database password
+        'HOST': 'localhost',  # Set to your database host
+        'PORT': '5432',  # Default PostgreSQL port
     }
 }
 
